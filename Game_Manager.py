88c1efa2473cc385ed1d716a51e75de5f19e26e8
YiminLin1994 <1192697976@qq.com>
2017-11-17 22:18:53 -0500

    typo in main

--- Game_Manager.py
+++ Game_Manager.py
@@ -1,6 +1,9 @@
 from utils import *
 from logic import *
 import random
+from sys import platform
+import keyboard
+
 
 keyboard_action = {'w': 'UP', 'W': 'UP', 's': 'DOWN', 'S': 'DOWN',
                    'a': 'LEFT', 'A': 'LEFT', 'd': 'RIGHT', 'D': 'RIGHT'}
@@ -132,7 +135,7 @@ def run_keyboard():
         print("current score is, ", curr_score)
         print("Possible actions: up, left, right, down, exit")
         print("Please press WASD for UP LEFT DOWN RIGHT Q for exit")
-        
+
         p = keyPress()
         if p == 'q' or p == 'Q':
             break
@@ -160,11 +163,63 @@ def run_keyboard():
     print("Game end")
     print("To run this game, type run_keyboard()")
 
+
+def runWin32():
+    board = make_board(4)
+    initial_two(board)
+    print_board(board)
+    curr_score = 0
+    while not check_end(board):
+        print(" ")
+        print("current score is, ", curr_score)
+        print("Possible actions: up, left, right, down, exit")
+        print("Please press WASD for UP LEFT DOWN RIGHT Q for exit")
+        flag = True
+        p = None
+        while flag:
+            try:
+                for i in keyboard_action:
+                    if keyboard.is_pressed(i):
+                        p = i
+                        flag = False
+                        break
+                for i in ["q", "Q"]:
+                    if keyboard.is_pressed(i):
+                        p = i
+                        flag = False
+                        break
+            except:
+                pass
+        if p == 'q' or p == 'Q':
+            break
+        action = keyboard_action[p]
+        action = action.upper()
+        if action == "EXIT":
+            break
+        # take action here to do the move
+        # and clear current , then print board
+        if can_move(board, action):
+            move(board, action)
+            curr_score = add_up(board, action, curr_score)
+            move(board, action)
+            clear()
+            simple_add_num(board)
+            print_board(board)
+        else:
+            clear()
+            print_board(board)
+    print("")
+    print("Game end")
+    print("To run this game, type run_keyboard()")
+
+
 if __name__ == "__main__":
-    from sys import platform
+
     if platform.startswith('linux') or platform == 'darwin':
         run_keyboard()
     elif platform == 'win32' or platform == 'cygwin':
-        run()
+        runWin32()
     else:
         run()
+
+
