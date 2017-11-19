98d6665d7d63137d10f05b620f4233338573b027
xuanyu <xychen233@gmail.com>
2017-11-19 14:19:31 -0500

    ..

--- gui.py
+++ gui.py
@@ -20,10 +20,10 @@ BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
 BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563",
                          32: "#f67c5f", 64: "#f65e3b", 128: "#edcf72", 256: "#edcc61",
-                         512: "#edc850", 1024: "#edc53f", 2048: "#edc22e"}
+                         512: "#edc850", 1024: "#edc53f", 2048: "#edc22e", 4096:"#edc22e", 8192:"#eff9c5",16384:"#11f29c"}
 CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
                    32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2", 256: "#f9f6f2",
-                   512: "#f9f6f2", 1024: "#f9f6f2", 2048: "#f9f6f2"}
+                   512: "#f9f6f2", 1024: "#f9f6f2", 2048: "#f9f6f2", 4096: "#f9f53e", 8192:"#f9f6f3",16384:"#f9f6f3"}
 FONT = ("Verdana", 40, "bold")
 
 moves = ('UP', 'DOWN', 'LEFT', 'RIGHT')
