de4147d4c63cb5592d985ea7007b4566b6b2a280
xuanyu <xychen233@gmail.com>
2017-12-03 17:56:21 -0500

    add get results for minimax

--- results.py
+++ results.py
@@ -3,6 +3,8 @@
 
 from algorithms.MCTS import naive_random_move
 from algorithms.expectimax import expectimax
+from algorithms.minimax import Minimax
+
 from core.utils import *
 from core.logic import *
 import copy
@@ -16,7 +18,7 @@ import copy
 # average_moves to end of game
 
 
-def get_results_mcts(which_algorithm = 'mcts', test_times = 100):
+def get_results_mcts(which_algorithm = 'mcts', test_times = 5):
     res = []
 
     total_score = {}
@@ -59,6 +61,9 @@ def get_results_mcts(which_algorithm = 'mcts', test_times = 100):
 
             average_moves += number_of_moves
 
+            # debug output
+            print("Finish {} iteration when number of test simulation is {}".format(i, sim_move))
+
         max_total_scores[sim_move] = max_total_score
 
         res.append(which_algorithm + ' with sim_move {}'.format(sim_move))
@@ -77,7 +82,7 @@ def get_results_mcts(which_algorithm = 'mcts', test_times = 100):
 
     return res
 
-def get_results_expectimax(which_algorithm = 'expectimax', test_times = 3):
+def get_results_expectimax(which_algorithm = 'expectimax', test_times = 20):
     moves = ('UP', 'DOWN', 'LEFT', 'RIGHT')
 
     res = []
@@ -137,6 +142,9 @@ def get_results_expectimax(which_algorithm = 'expectimax', test_times = 3):
                 max_total_score = curr_score
             average_moves += number_of_moves
 
+            # debug output
+            print("Finish {} iteration in depth {}".format(i, depth))
+
         res.append(which_algorithm + ' with depth {}'.format(depth))
         res.append(max_tiles)
         res.append('max score for this depth is {}'.format(max_total_score))
@@ -150,5 +158,62 @@ def get_results_expectimax(which_algorithm = 'expectimax', test_times = 3):
             out.write('\n')
 
     return res
+
+def get_results_minimax(which_algorithm='minimax', test_times=2):
+
+    res = []
+    total_score = {}
+
+    max_depths = (2,3,4,5,6,7,8,9,10,11,12,13,14)
+
+    for depth in max_depths:
+
+        max_tiles = {}
+        average_moves = 0
+        max_total_score = 0
+
+        for i in range(test_times):
+            board = make_board(4)
+            initial_two(board)
+            curr_score = 0
+            number_of_moves = 0
+
+            player = Minimax(board, depth)
+
+            while not check_end(board):
+                best_move = player.basic_move()
+                if can_move(board, best_move):
+                    number_of_moves += 1
+                    move(board,best_move)
+                    curr_score += add_up_v2(board,best_move)
+                    move(board,best_move)
+
+            max_tile = find_max_cell(board)
+            if max_tile not in max_tiles:
+                max_tiles[max_tile] = 1
+            else:
+                max_tiles[max_tile] += 1
+            if curr_score not in total_score:
+                total_score[curr_score] = 1
+            else:
+                total_score[curr_score] += 1
+            if max_total_score < curr_score:
+                max_total_score = curr_score
+            average_moves += number_of_moves
+
+        res.append(which_algorithm + ' with depth {}'.format(depth))
+        res.append(max_tiles)
+        res.append('max score for this depth is {}'.format(max_total_score))
+        res.append('average_moves_till_end: {}'.format(average_moves / test_times))
+        res.append('\n')
+
+    with open('results_{}.txt'.format(which_algorithm), mode='wt', encoding='utf-8') as out:
+        for e in res:
+            out.write(str(e))
+            out.write('\n')
+
+    return res
+
+
 
 # get_results_expectimax()
