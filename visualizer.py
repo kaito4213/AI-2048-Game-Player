852c8ac961958a79c6e89c38866c6594c596caec
YiminLin1994 <1192697976@qq.com>
2017-12-04 00:11:10 -0500

    Visualizer Available for Line(for "movements, score"), Grouped bar(for "movements, score") and StackedBar(for "max_tile")

--- visualizer.py
+++ visualizer.py
@@ -1,34 +1,160 @@
 import matplotlib.pyplot as plt
-import manager
 import numpy as np
+import random
+import plotly.plotly as py
+import plotly.graph_objs as go
+from core.gui import CELL_COLOR_DICT
+
+def test_generator(n):
+    '''
+    Generate a list contains n dictionaries.
+    {"movements: 1111,
+         "score": 111,
+        "max_tile": 2048,
+        "depth": 2}
+    :param n:
+    :return: a list of dictionary
+    '''
+    lst = []
+    for i in range(n):
+        temp = {}
+        for j in ["movements", "score", "depth"]:
+            temp[j] = random.randint(1,1000)
+        temp["max_tile"] = random.choice(list(CELL_COLOR_DICT.keys()))
+        lst.append(temp)
+    return lst
 
 class Visualizer:
-    def __init__(self, run_time = 2, mode = "score", algo = "expectimax"):
-        self.score_lst = []
-        self.cell_lst = []
-        self.x = []
-        self.run_time = run_time
-        self.mode = mode
-        self.algo = algo
+    def __init__(self, x_range = np.arange(1, 50)):
+        '''
 
-    def processing(self):
-        for i in range(self.run_time):
-            best_score, max_cell = manager.run(mode = self.mode, algo = self.algo)
-            self.score_lst.append(best_score)
-            self.cell_lst.append(max_cell)
-            self.x.append(i+1)
-        # Count the frequency of max cell value
-        self.unique_cell, self.unique_count = np.unique(self.cell_lst)
+        :param x_range: The range of x
+        '''
+        self.x_range = x_range
+        self.dataset = {}
 
-    def get_score(self):
-        return np.average(self.score_lst)
+    def add_data(self, label, data):
+        '''
+        :param label: a str identify the name of the data.
+        :param data: should be a 1D array that  has the same length to x_range
+        :return: None
+        '''
+        if len(data) != len(self.x_range):
+            Warning("The length of data is {}, which is not the same as the length of x: {}".format(len(data), len(self.x_range)))
+            if len(data)< len(self.x_range):
+                for i in range(len(self.x_range) - len(data)):
+                    data.append(0)
+            else:
+                data = data[:len(self.x_range)]
+        self.dataset[label] = data
 
-    def visualize(self):
-        plt.hist(self.unique_cell, self.unique_count, facecolor = "g", alpha = 0.75)
-        plt.xlabel("Max Cell")
-        plt.ylabel("Counts")
-        plt.show()
+    def get_value_by_key(self, key):
+        lst = []
+        labels = []
+        for label in self.dataset:
+            temp = []
+            labels.append(label)
+            for unit in self.dataset[label]:
+                temp.append(unit[key])
+            lst.append(temp)
+        return lst, labels
 
-a = Visualizer()
-a.processing()
-a.visualize()
+    def get_max(self, matrix):
+        '''
+        For each row in matrix, get the max in each row
+        :param matrix:
+        :return:
+        '''
+        max_lst = []
+        for row in matrix:
+            max_lst.append(max(row))
+        return max_lst
+
+    def get_average(self, matrix):
+        av_lst = []
+        for row in matrix:
+            av_lst.append(np.average(row))
+        return av_lst
+
+    def draw_lines(self, key):
+        lines = []
+        data_of_key, labels = self.get_value_by_key(key)
+        for i in range(len(data_of_key)):
+            trace = go.Scatter(
+                x= self.x_range,
+                y= data_of_key[i],
+                mode='lines+markers',
+                name=labels[i]
+            )
+            lines.append(trace)
+        layout = dict(title = "{} for Different Algorithms".format(key),
+                      xaxis = dict(title = "Run Time"),
+                      yaxis = dict(title = key))
+        fig = dict(data = lines, layout = layout)
+        return py.iplot(fig, filename = "Line of {}".format(key))
+
+    def draw_grouped_bar(self, key):
+        data_of_key, labels = self.get_value_by_key(key)
+        best_lst = self.get_max(data_of_key)
+        aver_lst = self.get_average(data_of_key)
+
+        trace0 = go.Bar(
+            x=labels,
+            y=best_lst,
+            name='Best {}'.format(key)
+        )
+
+        trace1 = go.Bar(
+            x=labels,
+            y=aver_lst,
+            name='Average {}'.format(key)
+        )
+        bars = [trace0, trace1]
+        layout = go.Layout(
+            barmode = 'group',
+            title = "{} for Different Algorithms".format(key),
+            yaxis=dict(title=key)
+        )
+        fig = dict(data = bars, layout = layout)
+        return py.iplot(fig, filename = "Line of {}".format(key))
+
+    def draw_stack_bar(self, key):
+        count_matrix = []
+        data_of_key, labels = self.get_value_by_key(key)
+        all_uniques_cells = sorted(list(CELL_COLOR_DICT.keys()))
+        for i in range(len(data_of_key)):
+            uniques, counts = np.unique(data_of_key[i], return_counts= True)
+            temp = []
+
+            # sum_counts = np.sum(counts)
+            # counts = counts/sum_counts
+            i = 0
+            for j in all_uniques_cells:
+                if j != uniques[i]:
+                    temp.append(0)
+                else:
+                    temp.append(counts[i])
+                    i += 1
+            count_matrix.append(temp)
+        np_matrix = np.array(count_matrix)
+        np_matrix = np_matrix.T
+        stacked_data = []
+        for i in range(len(np_matrix)):
+            trace0 = go.Bar(
+                x = labels,
+                y = np_matrix[i],
+                name = all_uniques_cells[i],
+                # marker = dict(
+                #     color = CELL_COLOR_DICT[all_uniques_cells[i]]
+                # )
+            )
+            stacked_data.append(trace0)
+
+        layout = go.Layout(
+            barmode = 'stack'
+        )
+        fig = go.Figure(data=stacked_data, layout=layout)
+        return py.iplot(fig, filename='stacked-bar')
+
+
+
